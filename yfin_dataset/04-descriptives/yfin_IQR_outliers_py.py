0055f01f47283c7c53f990d0cb15be03e809e17f
Alex <alexandercrockett@gmail.com>
2023-12-20 22:39:26 -0800

    Renaming sets

--- yfin_dataset/04-descriptives/yfin_IQR_outliers_py.py
+++ yfin_dataset/04-descriptives/yfin_IQR_outliers_py.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Notes (IQR)
-# 
-# All we are going to do in this file is compute the inter-quartile-range and create a variable that references out data without these outliers. We're also going to create a third variable that excludes companies along with the IQR.
-# 
-# For each, we will also compute a measure of skew to see how the data are shaped.
-# 
-# As a note, the IQR is:
-# $\text{IQR} = Q3 - Q1$
-# 
-# Outliers are considered:
-# $Q1 - \text{IQR} \cdot 1.5$ and $Q3 + \text{IQR} \cdot 1.5$ 
-
-# In[2]:
-
-
 # Importing libraries and variables
 
 import pandas as pd
 yfin_csv = pd.read_csv(r'https://raw.githubusercontent.com/alexcrockett/Jupyter-Playground/personal/yfin_dataset/02-data/stock_details_5_years.csv')
-from yfin_group_range_py import company_averages, first_quartile, fourth_quartile, companies_in_first_quartile, companies_in_fourth_quartile
-
-
-# In[3]:
-
-
-# Defining the IQR
+from yfin_group_range_py import company_averages, first_quartile, fourth_quartile
 
 ## Define the 3rd Quartile and median
 median = company_averages.quantile(0.5)  # The median (50th percentile)
@@ -39,9 +18,6 @@ companies_in_third_quartile = company_averages[(company_averages > median) & (co
 IQR = third_quartile - first_quartile
 
 print("IQR= ", float(str(IQR)))
-
-
-# In[4]:
 
 
 # Finding outliers
@@ -60,9 +36,6 @@ outlier_upper_bound = third_quartile + outlier_scope
 
 ## Identify outliers below the lower bound
 outliers_upper = company_averages[company_averages > outlier_upper_bound]
-
-
-# In[5]:
 
 
 # Review the results
@@ -86,29 +59,12 @@ print("Outliers in the upper bound:")
 print(outliers_upper)
 print("-------------------")
 
-
-# In[6]:
-
-
-# Create a new dataframe without the outliers
-
 ## Map companies to outliers
 outlier_companies = outliers_upper.index
 
 ## Remove the comapnies
 yfin_restricted_set = yfin_csv[~yfin_csv['Company'].isin(outlier_companies)]
 
-
-# # Notes
-# `outliers_upper.index` gives you the names of the companies that are outliers.
-# 
-# `yfin_csv['Company'].isin(outlier_companies)` creates a boolean mask where each row is `True` if the company is in the list of outliers and `False` otherwise.
-# 
-# The `~` operator negates the boolean mask, so you select all rows where the company is not an outlier.
-# 
-# `yfin_csv[...]` with this mask removes the rows corresponding to outlier companies.
-
-# In[7]:
 
 
 # Revaluate the mean and median scores
@@ -140,22 +96,6 @@ central_tendency_restricted = {
 # Print the results
 for key, value in central_tendency_restricted.items():
     print(f"{key}: {value:.2f}")
-
-
-# # Notes
-# 
-# We can see that the gap between mean and median scores has radiacally reduced by removing ~35 companies from the set of 500. 
-# 
-# This gives us the data-sets
-# - yfin_csv
-# - yfin_restricted_set
-# 
-# We want one more set, a set without the outliers and without scores that have extreme ranges. We will also want to rename our datasets so they're easier to type. We will call them 
-# - set_1
-# - set_2
-# - set_3
-
-# In[14]:
 
 
 # Finding range outliers
@@ -192,9 +132,6 @@ outlier_companies_count = outlier_companies.shape
 yfin_restricted_range = yfin_csv[~yfin_csv['Company'].isin(outlier_companies)]
 
 
-# In[15]:
-
-
 # Print the results
 print("-------------------")
 print(" ")
@@ -219,22 +156,9 @@ print(outlier_companies)
 
 print("count: ", outlier_companies_count)
 
-
-# There are 306 companies in this list, which suggests a serious problem with the method. The result seems to indicate that based on the calculations, most of the dataset falls outside the IQR, which indicates either, the method os is not useful in this context.
-# 
-# One interpretation is that segmenting the company stock market prices and using this method for financial data like this will lead to a large number of false postives due to the thickness of the tails inherent in this kind of data. It is also possible that given that the data stretches from 2018 to 2023, both the Pandemic and the 2020 election will have played a role. These are testable hypotheses and graphical analysis of the data will help determine if more specificity in the procedure, or extending the bounds for outliers will be useful.
-
-# In[ ]:
-
-
 # Renaming datasets
 
-set_1 = yfin_csv # original data set
-set_2 = yfin_restricted_set # data without outliers
-
-
-# In[ ]:
-
-
+set_1 = yfin_csv.copy # A copy of the original data
+set_2 = yfin_restricted_set.copy # A copy of the data without outliers
 
 
